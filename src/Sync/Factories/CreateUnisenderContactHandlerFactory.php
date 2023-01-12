5e540658b8d1d74b22bd901ec621e73f9f9df282
azabolotsky <azabolotsky@team.amocrm.com>
2023-01-12 14:28:13 +0000

    Revert "Merge branch 'createContactUnisender' into 'master'"
    This reverts merge request !2

--- src/Sync/Factories/CreateUnisenderContactHandlerFactory.php
+++ src/Sync/Factories/CreateUnisenderContactHandlerFactory.php
@@ -4,13 +4,13 @@ namespace Sync\Factories;
 
 use Psr\Container\ContainerInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Sync\Handlers\ImportContactsHandler;
+use Sync\Handlers\CreateUnisenderContactHandler;
 use Sync\Handlers\WebhookHandler;
 
 class CreateUnisenderContactHandlerFactory
 {
     public function __invoke(ContainerInterface $container): RequestHandlerInterface
     {
-        return new ImportContactsHandler();
+        return new CreateUnisenderContactHandler();
     }
 }
